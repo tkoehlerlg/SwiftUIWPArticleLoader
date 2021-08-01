4dafe4111ded3952594e86e7201d4122ed501010
Torben Köhler <t.koehler@schueler.lg-ks.de>
2021-08-01 04:14:30 +0200

    Update ArticleLoadingService.swift

--- Sources/SwiftUIWPArticleLoader/Services/ArticleLoadingService.swift
+++ Sources/SwiftUIWPArticleLoader/Services/ArticleLoadingService.swift
@@ -20,8 +20,19 @@ struct ArticleLoadingService {
                     do {
                         let articles = try decoder.decode([ContentfulArticle].self, from: data)
                         promise(.success(articles.map({ Article(from: $0) })))
+                    } catch DecodingError.dataCorrupted(let context) {
+                        print(context)
+                    } catch DecodingError.keyNotFound(let key, let context) {
+                        print("Key '\(key)' not found:", context.debugDescription)
+                        print("codingPath:", context.codingPath)
+                    } catch DecodingError.valueNotFound(let value, let context) {
+                        print("Value '\(value)' not found:", context.debugDescription)
+                        print("codingPath:", context.codingPath)
+                    } catch DecodingError.typeMismatch(let type, let context) {
+                        print("Type '\(type)' mismatch:", context.debugDescription)
+                        print("codingPath:", context.codingPath)
                     } catch {
-                        promise(.failure(error))
+                        print("error: ", error)
                     }
                 }.resume()
             }
