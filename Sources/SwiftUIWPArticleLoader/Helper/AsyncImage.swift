3979f215a3e88a73e21dfd4465c25a3699965a82
Torben Köhler <t.koehler@schueler.lg-ks.de>
2021-08-06 10:40:39 +0200

    added AsyncImage

---
+++ Sources/SwiftUIWPArticleLoader/Helper/AsyncImage.swift
@@ -0,0 +1,42 @@
+//
+//  AsyncImage.swift
+//  Clickbait ML Multi
+//
+//  Created by Torben Köhler on 13.06.21.
+//
+
+import SwiftUI
+import Combine
+import Foundation
+
+#if !os(macOS)
+struct AsyncImage<Placeholder: View>: View {
+    @StateObject private var loader: ImageLoader
+    private let placeholder: Placeholder
+
+    init(url: URL, @ViewBuilder placeholder: () -> Placeholder) {
+        self.placeholder = placeholder()
+        _loader = StateObject(wrappedValue: ImageLoader(url: url))
+    }
+
+    init(imageLoader: ImageLoader, @ViewBuilder placeholder: () -> Placeholder) {
+        self.placeholder = placeholder()
+        _loader = StateObject(wrappedValue: imageLoader)
+    }
+
+    var body: some View {
+        content
+    }
+
+    private var content: some View {
+        Group {
+            if loader.image != nil {
+                Image(uiImage: loader.image!)
+                    .resizable()
+            } else {
+                placeholder
+            }
+        }
+    }
+}
+#endif
