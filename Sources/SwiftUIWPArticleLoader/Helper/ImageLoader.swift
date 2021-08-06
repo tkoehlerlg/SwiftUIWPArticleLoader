6ac6c9847f198136b29d233fd228a86028318508
Torben Köhler <t.koehler@schueler.lg-ks.de>
2021-08-06 10:36:01 +0200

    Update ImageLoader.swift

--- Sources/SwiftUIWPArticleLoader/Helper/ImageLoader.swift
+++ Sources/SwiftUIWPArticleLoader/Helper/ImageLoader.swift
@@ -5,23 +5,31 @@
 //  Created by Torben Köhler on 31.07.21.
 //
 
+import Foundation
 import Combine
 #if !os(macOS)
 import UIKit
+#endif
 
+@available(macOS 10.15, *)
 public class ImageLoader: ObservableObject {
     private var cancellable: AnyCancellable?
+    #if !os(macOS)
     @Published public var image: UIImage?
-    private let url: URL
+    #endif
+    public let url: URL
     // MARK: init
     public init(url: URL) {
         self.url = url
+        #if !os(macOS)
         load()
+        #endif
     }
     deinit {
         cancel()
     }
     // MARK: reload
+    #if !os(macOS)
     public func reload() { load() }
     private func load() {
         cancellable = URLSession.shared.dataTaskPublisher(for: url)
@@ -30,8 +38,9 @@ public class ImageLoader: ObservableObject {
             .receive(on: DispatchQueue.main)
             .sink { [weak self] in self?.image = $0 }
     }
+    #endif
     func cancel() {
         cancellable?.cancel()
     }
 }
-#endif
+
