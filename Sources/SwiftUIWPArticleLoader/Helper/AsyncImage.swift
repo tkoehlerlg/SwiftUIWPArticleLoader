c43b98de9b8fe13d86dbc06f7f2f0b5eb8ad5901
Torben Köhler <t.koehler@schueler.lg-ks.de>
2021-08-08 11:59:25 +0200

    clean code of AsyncImage

--- Sources/SwiftUIWPArticleLoader/Helper/AsyncImage.swift
+++ Sources/SwiftUIWPArticleLoader/Helper/AsyncImage.swift
@@ -14,12 +14,11 @@ import Foundation
 public struct AsyncImage<Placeholder: View>: View {
     @StateObject private var loader: ImageLoader
     private let placeholder: Placeholder
-
+    // MARK: init
     public init(url: URL, @ViewBuilder placeholder: () -> Placeholder) {
         self.placeholder = placeholder()
         _loader = StateObject(wrappedValue: ImageLoader(url: url))
     }
-
     public init(imageLoader: ImageLoader, @ViewBuilder placeholder: () -> Placeholder) {
         self.placeholder = placeholder()
         _loader = StateObject(wrappedValue: imageLoader)
@@ -28,7 +27,6 @@ public struct AsyncImage<Placeholder: View>: View {
     public var body: some View {
         content
     }
-
     private var content: some View {
         Group {
             if loader.image != nil {
